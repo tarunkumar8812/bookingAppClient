9cf3928bfaa0fddd94504b41d4cf3846997754a3
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-03-07 16:21:27 +0530

    routes created

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
 
+  return (
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} ></Route>
+        <Route path="/hotels" element={<List />} ></Route>
+        <Route path="/hotels/:id" element={<Hotel />} ></Route>
+        <Route path="/login" element={<Login />} ></Route>
+        <Route path="/register" element={<Register />} ></Route>
+      </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
