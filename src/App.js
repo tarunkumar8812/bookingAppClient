3983f1db815bf532e069dff668711e3b43f32987
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-03-07 15:25:31 +0530

    navbar is created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import './App.css';
-import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <>
-      <div className="App">
-        this is change what i want to be
-      </div>
-      <Navbar></Navbar>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} ></Route>
+        </Routes>
+      </BrowserRouter>
+
     </>
   );
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+
+import Navbar from '../../components/navbar/Navbar'
+import './home.css'
+
+const Home = () => {
+
+    return (
+        <>
+            <div>
+                <Navbar />
+                <div className='homeContainer'>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
