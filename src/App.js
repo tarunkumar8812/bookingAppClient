f9c13ed09084959d3b5abf8c23d90ccf024ac162
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-03-07 15:07:58 +0530

    navbar file is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      this is change what i want to be
-    </div>
+    <>
+      <div className="App">
+        this is change what i want to be
+      </div>
+      <Navbar></Navbar>
+    </>
   );
 }
 
