2af0e2b9eb020ca1c941d418367e11c202f09488
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-03-08 21:47:06 +0530

    correction in index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/Home";
-import Hotel from "./pages/hotel/Hotel";
-import List from "./pages/list/List";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AuthContextProvider } from './context/AuthContext';
+import { SearchContextProvider } from './context/SearchContext';
+import './index.css';
+// import {Route} from 'react-router-dom';
 
-function App() {
-
-  return (
-
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} ></Route>
-        <Route path="/hotels" element={<List />} ></Route>
-        <Route path="/hotels/:id" element={<Hotel />} ></Route>
-        <Route path="/login" element={<Login />} ></Route>
-        <Route path="/register" element={<Register />} ></Route>
-      </Routes>
-    </BrowserRouter>
-
-  );
-}
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <AuthContextProvider>
+      <SearchContextProvider>
+        <App />
+      </SearchContextProvider>
+    </AuthContextProvider>
+  </React.StrictMode>
+);
